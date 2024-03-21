a7b921d7475c4ace9bb8b1692a4e2a724bda9633
laurakond <laura.kondrataite@gmail.com>
2024-03-21 16:28:14 +0000

    Final tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    document.getElementById("answer-box").addEventListener("keydown", function(event){
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
-    })
+    });
 
     runGame("addition");
 
